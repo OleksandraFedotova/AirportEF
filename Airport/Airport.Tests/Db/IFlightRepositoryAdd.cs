30cb7dc28777a2e756da375c1588aa2370dc97a7
OleksandraFedotova <feyasasha19111996@gmail.com>
2018-07-18 04:15:42 +0300

    added tests

---
+++ Airport/Airport.Tests/Db/IFlightRepositoryAdd.cs
@@ -0,0 +1,58 @@
+﻿using Airport.Domain.Repositiories;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport.Tests.Db
+{
+   public class IFlightRepositoryAdd : BaseDBScenario
+    {
+        private readonly Guid _id = Guid.NewGuid();
+
+        protected override void Given()
+        {
+
+        }
+
+        protected override async Task When()
+        {
+            var repo = Resolve<IFlightRepository>();
+
+            await repo.Create(new Domain.Entities.Flight
+            {
+                Id = _id,
+                DeparturePoint="Kyiv",
+                DepartureTime=DateTime.Today,
+                Destination="Dnipro",
+                Number=1,
+                TimeOfArrival=DateTime.Today
+            });
+        }
+
+        [Test]
+        public async Task AirCraftTypeAdded()
+        {
+            var repo = Resolve<IFlightRepository>();
+
+            var flight = await repo.GetById(_id);
+
+            Assert.IsNotNull(flight);
+
+            Assert.AreEqual("Kyiv", flight.DeparturePoint);
+            Assert.AreEqual(DateTime.Today, flight.DepartureTime);
+            Assert.AreEqual(1, flight.Number);
+            Assert.AreEqual("Dnipro", flight.Destination);
+            Assert.AreEqual(DateTime.Today, flight.TimeOfArrival);
+        }
+
+        [TearDown]
+        public async Task Clear()
+        {
+            var repo = Resolve<IAirCraftTypeRepository>();
+
+            await repo.Delete(_id);
+        }
+    }
+}
---
+++ Airport/Airport.Tests/Db/IStewardessRepositoryAdd.cs
@@ -0,0 +1,56 @@
+﻿using Airport.Domain.Repositiories;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport.Tests.Db
+{
+  public  class IStewardessRepositoryAdd : BaseDBScenario
+    {
+        private readonly Guid _id = Guid.NewGuid();
+
+        protected override void Given()
+        {
+
+        }
+
+        protected override async Task When()
+        {
+            var repo = Resolve<IStewardessRepository>();
+
+            await repo.Create(new Domain.Entities.Stewardess
+            {
+                Id = _id,
+                FirstName = "A",
+                LastName = "B",
+                DateOfBirth = DateTime.Today
+            });
+        }
+
+        [Test]
+        public async Task AirCraftTypeAdded()
+        {
+            var repo = Resolve<IPilotRepository>();
+
+            var stewardess = await repo.GetById(_id);
+
+            Assert.IsNotNull(stewardess);
+
+            Assert.AreEqual("A", stewardess.FirstName);
+
+            Assert.AreEqual("B", stewardess.LastName);
+
+            Assert.AreEqual(DateTime.Today, stewardess.DateOfBirth);
+        }
+
+        [TearDown]
+        public async Task Clear()
+        {
+            var repo = Resolve<IAirCraftTypeRepository>();
+
+            await repo.Delete(_id);
+        }
+    }
+}
